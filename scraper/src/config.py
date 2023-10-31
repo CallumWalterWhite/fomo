52c85d3865465a42759a2f9bd41191193a37639c
a-whitec <callum.white@mhr.co.uk>
2023-10-31 21:56:03 +0000

    wip

--- scraper/src/config.py
+++ scraper/src/config.py
@@ -1,8 +1,8 @@
 queries = [
     {
-        "keyword": "bars pubs and nightclubs in nottingham",
-        "max_results" : 150,
+        "keyword": "bars pubs and nightclubs in derby",
+        "max_results" : 75,
     },
 ]
 number_of_scrapers = 16
-city_id = 2
+city_id = 1
