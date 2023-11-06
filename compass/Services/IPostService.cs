5fba08417bcf127f89cd4d9275e5ddbb95cd4433
Callum White <callumwhi@live.co.uk>
2023-11-06 00:05:44 +0000

    update

--- compass/Services/IPostService.cs
+++ compass/Services/IPostService.cs
@@ -5,4 +5,10 @@ namespace compass.Services;
 public interface IPostService
 {
     Post GetPost(string id);
+    
+    List<Post> GetPosts(string locationId);
+    
+    void CreatePost(Post post);
+    
+    void DeletePosts(Post post);
 }
--- compass/Services/PostService.cs
+++ compass/Services/PostService.cs
@@ -17,4 +17,19 @@ public class PostService : IPostService
         Post post = _postRepository.Get(id);
         return post;
     }
+
+    public List<Post> GetPosts(string locationId)
+    {
+        return _postRepository.Get((post) => post.LocationDataId == locationId);
+    }
+
+    public void CreatePost(Post post)
+    {
+        _postRepository.Create(post);
+    }
+
+    public void DeletePosts(Post post)
+    {
+        _postRepository.Delete(post);
+    }
 }
