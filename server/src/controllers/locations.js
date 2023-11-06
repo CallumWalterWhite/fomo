26a2651ee6ef5d567a80f0959d61c50d5a9f046c
a-whitec <callum.white@mhr.co.uk>
2023-11-06 23:31:22 +0000

    cs search

--- server/src/controllers/locations.js
+++ server/src/controllers/locations.js
@@ -24,7 +24,7 @@ export const getLocation = async (req, res) => {
 export const getLocationSearch = async (req, res) => {
   try {
     const { term } = req.params;
-    const locations = await Location.find({ sarchterm: { $regex: `.*${term.toLowerCase()}.*` } }).limit(10);
+    const locations = await Location.find({ sarchterm: { $regex: '.*' + term.toLowerCase() + '.*', $options: 'i' } }).limit(10);
     res.status(200).json(locations);
   } catch (err) {
     res.status(404).json({ message: err.message });
